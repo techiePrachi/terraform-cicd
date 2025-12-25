693a1dbd87df2a00d7b06d8f1a270b497fc07652
Prachi Khandelwal <prachik9202@gmail.com>
2025-12-25 16:08:45 +0530

    terraform ci cd pipeline

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform{
+    required_providers{
+        aws = {
+            source = "harshicorp/aws"
+            version = "~>5.0"
+        }
+    }
+}
+
+provider "aws"{
+    region= "us-east-1"
+
+}
+
+resource "aws_vpc" "this"{
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "cicd-vpc"
+    }
+}
+
+resource "aws_subnet" "this"{
+    vpc_id = aws_vpc.this.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "${var.region}a"
+}
+
+resource "aws_instance" "web"{
+    ami = var.ami
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.this.id
+    tags = {
+        Name = "terraform-cicd-demo"
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "ami"{
+    default = "ami-068c0051b15cdb816"
+}
